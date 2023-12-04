f8c8e6ffec6a92d7619001299ad2bf19131d98cf
Jeff Hanke <geophree@gmail.com>
2023-12-04 14:32:58 -0800

    Solve day 4 part 1.

---
+++ src/bin/04.rs
@@ -0,0 +1,67 @@
+advent_of_code::solution!(4);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut input = input.as_bytes().iter();
+    let mut total = 0;
+    loop {
+        let mut saw_colon = false;
+        for &c in input.by_ref() {
+            if c == b':' {
+                saw_colon = true;
+                break;
+            }
+        }
+
+        if !saw_colon {
+            break Some(total);
+        }
+
+        let mut winners = vec![];
+        loop {
+            input.next();
+            match (input.next(), input.next()) {
+                (Some(b'|'), _) => break,
+                (Some(a), Some(b)) => {
+                    winners.push(u32::from_ne_bytes([0, 0, *a, *b]));
+                }
+                _ => break,
+            }
+        }
+
+        let mut matches = 0;
+        loop {
+            let (Some(a), Some(b)) = (input.next(), input.next()) else {
+                break;
+            };
+            matches += u32::from(winners.contains(&u32::from_ne_bytes([0, 0, *a, *b])));
+            if input.next() == Some(&b'\n') {
+                break;
+            }
+        }
+
+        if matches > 0 {
+            total += 2u32.pow(matches - 1);
+        }
+    }
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
