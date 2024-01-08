c2ac4d4b12433a96a536034110a32336cd14d198
Jeff Hanke <geophree@gmail.com>
2024-01-07 22:25:33 -0800

    Optimize day 9 part 2.

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -47,58 +47,45 @@ pub fn part_one(input: &str) -> Option<i32> {
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
-    let mut nums: Vec<Vec<i32>> = vec![];
-    for line in input.lines() {
-        let mut seq = vec![];
-        for num in line.split(' ') {
-            seq.push(num.parse().ok()?);
-        }
-        nums.push(seq);
-    }
-    let mut traps = vec![];
-    for seq in nums {
-        let mut trap = vec![];
-        trap.push(seq);
-        while trap.last()?.iter().any(|x| *x != 0) {
-            trap.push(
-                trap.last()?
-                    .iter()
-                    .scan(0, |p, n| {
-                        let pp = *p;
-                        *p = *n;
-                        Some(*n - pp)
-                    })
-                    .skip(1)
-                    .collect(),
-            );
-        }
-        traps.push(trap);
-    }
-    let mut firsts_lasts: Vec<Vec<_>> = vec![];
-    for seqs in &traps {
-        let mut fls = vec![];
-        for seq in seqs {
-            fls.push((*(seq.first()?), *(seq.last()?)));
-        }
-        firsts_lasts.push(fls);
-    }
+    let mut input = input.as_bytes().iter();
 
-    let mut prev_next = vec![];
-    for fls in &firsts_lasts {
-        let mut prev = 0;
-        let mut next = 0;
-        for (first, last) in fls.iter().rev() {
-            prev = *first - prev;
-            next += *last;
-        }
-        prev_next.push((prev, next));
-    }
-    let (total_prev, _total_next) = prev_next
-        .iter()
-        .copied()
-        .reduce(|(tp, tn), (prev, next)| (tp + prev, tn + next))?;
+    let mut total = 0;
+    loop {
+        let mut leftmost = vec![];
+        let mut differences = vec![];
+        let mut number = 0;
+        let mut sign = 1;
+        for c in input.by_ref() {
+            if c.is_ascii_digit() {
+                number *= 10;
+                number += i32::from(*c - b'0');
+                continue;
+            }
+            if *c == b'-' {
+                sign = -1;
+                continue;
+            }
+            number *= sign;
 
-    Some(total_prev)
+            for n in &mut differences {
+                (*n, number) = (number, number - *n);
+            }
+            leftmost.push(number);
+            differences.push(number);
+            if *c == b'\n' {
+                break;
+            }
+            number = 0;
+            sign = 1;
+        }
+
+        let before_first = leftmost.into_iter().rev().reduce(|m, n| n - m);
+        if let Some(before_first) = before_first {
+            total += before_first;
+        } else {
+            break Some(total);
+        }
+    }
 }
 
 #[cfg(test)]
